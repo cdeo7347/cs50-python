8125660fccd731e58e10ab647ca8e8aa9fbcf967
Caroline Deojay <56861997+cdeo7347@users.noreply.github.com>
2020-07-10 21:49:55 +0200

    Add files via upload

---
+++ mario.py
@@ -0,0 +1,14 @@
+from cs50 import get_int
+
+while True:
+    height = get_int("Height: ")
+    if height >= 1 and height <= 8:
+        break
+
+for i in range(height):
+    for j in range(height):
+        if i + j >= height - 1:
+            print("#", end="")
+        else:
+            print(" ", end="")
+    print()
